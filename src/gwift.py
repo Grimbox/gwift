5995cc804baa5ce1205b11bda57bc6ff9557e7e3
Fred Pauchet <fred@grimbox.be>
2014-05-26 20:44:25 +0200

    refactors db access

--- src/gwift.py
+++ src/gwift.py
@@ -1,13 +1,50 @@
 ﻿#coding=utf-8
 from flask import Flask, render_template
 from flask_peewee.rest import RestAPI, RestResource, UserAuthentication
-from auth import auth
+from flask_peewee.auth import Auth
+from flask_peewee.auth import BaseUser
+from flask_peewee.db import Database
+from peewee import SqliteDatabase, Model, CharField, DateField, ForeignKeyField, DateTimeField, BooleanField, DecimalField, IntegerField
 
 from api_resources import ItemResource, UserResource
-from models import WishList, Item, Part, User
+
+import datetime
+
+DATABASE = {
+	'name' : 'example.db',
+	'engine' : 'peewee.SqliteDatabase',
+}
+
+DEBUG = True
 
 app = Flask(__name__, static_folder='static', static_url_path='') # création de l'appli Flask
-	
+app.config.from_object(__name__)
+
+db = Database(app)
+
+class UserBidule(db.Model):
+	name = CharField()
+
+class WishList(db.Model):
+	name = CharField()
+	description = CharField()
+	validity_date = DateField()
+	user = ForeignKeyField(UserBidule, related_name='wishlists')
+
+class Item(db.Model):
+	name = CharField()
+	description = CharField(max_length=2000, null=True)
+	url = CharField()
+	price = DecimalField(max_digits=10, decimal_places=2)
+	wishlist = ForeignKeyField(WishList, related_name='items')
+	numberOfParts = IntegerField()
+
+class Part(db.Model):
+	user = ForeignKeyField(UserBidule, related_name='gifts')
+	item = ForeignKeyField(Item, related_name='parts')
+
+auth = Auth(app, db)
+
 user_auth = UserAuthentication(auth)
 
 api = RestAPI(app, default_auth=user_auth)
@@ -15,7 +52,7 @@ api = RestAPI(app, default_auth=user_auth)
 api.register(WishList)
 api.register(Item, ItemResource)
 api.register(Part)
-api.register(User, UserResource)
+api.register(UserBidule, UserResource)
 
 api.setup()
 
@@ -24,4 +61,12 @@ def index():
 	return render_template('index.html')
 	
 if __name__ == '__main__':	 
+	auth.User.create_table(fail_silently=True)
+	
+	UserBidule.create_table(fail_silently=True)
+	WishList.create_table(fail_silently=True)
+	Item.create_table(fail_silently=True)
+	Part.create_table(fail_silently=True)
+	
+	
 	app.run(debug=True)
